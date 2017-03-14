119b3e0c93b4712e35395c2ca54297ed0e5f061b
Shik <zkgengkun@163.com>
2017-03-14 18:32:04 +0800

    first

---
+++ spring-cloud/shik-cloudclient/src/main/java/com/shik/client/ComputeClient.java
@@ -0,0 +1,40 @@
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+package com.shik.client;
+
+import com.shik.client.hystrix.ComputeClientHystrix;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author gengshikun
+ * @date 2017/3/14
+ */
+@FeignClient(value = "compute-service", fallback = ComputeClientHystrix.class)
+public interface ComputeClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+
+}
---
+++ spring-cloud/shik-cloudclient/src/main/java/com/shik/client/hystrix/ComputeClientHystrix.java
@@ -0,0 +1,37 @@
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+package com.shik.client.hystrix;
+
+import com.shik.client.ComputeClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author gengshikun
+ * @date 2017/3/14
+ */
+@Component
+public class ComputeClientHystrix implements ComputeClient {
+    public Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
+        return null;
+    }
+}
