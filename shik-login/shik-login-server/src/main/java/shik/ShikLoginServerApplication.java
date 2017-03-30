12c7475d7c7ff3104c49b87453b4d4886cc96074
Shik <zkgengkun@163.com>
2017-03-30 11:10:45 +0800

    config-server  8888

---
+++ shik-login/shik-login-server/src/main/java/shik/ShikLoginServerApplication.java
@@ -0,0 +1,37 @@
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+package shik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gengshikun
+ * @date 2017/3/24
+ */
+@SpringBootApplication
+public class ShikLoginServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShikLoginServerApplication.class);
+    }
+}
