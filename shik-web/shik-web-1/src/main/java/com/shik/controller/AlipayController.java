e2450398f6f287044f1abbbd7b7b7c56398a6983
Shik <zkgengkun@163.com>
2017-07-18 15:16:08 +0800

    alipay

--- shik-web/shik-web-1/src/main/java/com/shik/controller/AlipayController.java
+++ shik-web/shik-web-1/src/main/java/com/shik/controller/AlipayController.java
@@ -25,6 +25,7 @@ import com.shik.client.ShikPayClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -39,6 +40,7 @@ public class AlipayController {
 
     private final Logger logger = LoggerFactory.getLogger(AlipayController.class);
 
+    @Qualifier("shik-zuul-gateway")
     @Autowired
     private ShikPayClient shikPayClient;
 
