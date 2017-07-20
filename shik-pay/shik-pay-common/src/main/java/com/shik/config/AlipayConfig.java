bb9ac00472246c8b53fa149e0d036f64a7e8031c
Shik <zkgengkun@163.com>
2017-07-20 10:17:53 +0800

    alipay参数放入 config仓

--- shik-pay/shik-pay-common/src/main/java/com/shik/config/AlipayConfig.java
+++ shik-pay/shik-pay-common/src/main/java/com/shik/config/AlipayConfig.java
@@ -34,7 +34,6 @@ import org.springframework.context.annotation.PropertySource;
  */
 @Configuration
 @ConfigurationProperties(prefix = "alipay")
-@PropertySource("classpath:config/alipay.properties")
 public class AlipayConfig {
 
     @Bean(name = "alipayClient")
@@ -59,67 +58,4 @@ public class AlipayConfig {
 
     private String sign_type;
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getApp_id() {
-        return app_id;
-    }
-
-    public void setApp_id(String app_id) {
-        this.app_id = app_id;
-    }
-
-    public String getApp_private_key() {
-        return app_private_key;
-    }
-
-    public void setApp_private_key(String app_private_key) {
-        this.app_private_key = app_private_key;
-    }
-
-    public String getFormat() {
-        return format;
-    }
-
-    public void setFormat(String format) {
-        this.format = format;
-    }
-
-    public String getCharset() {
-        return charset;
-    }
-
-    public void setCharset(String charset) {
-        this.charset = charset;
-    }
-
-    public String getApp_public_key() {
-        return app_public_key;
-    }
-
-    public void setApp_public_key(String app_public_key) {
-        this.app_public_key = app_public_key;
-    }
-
-    public String getAlipay_public_key() {
-        return alipay_public_key;
-    }
-
-    public void setAlipay_public_key(String alipay_public_key) {
-        this.alipay_public_key = alipay_public_key;
-    }
-
-    public String getSign_type() {
-        return sign_type;
-    }
-
-    public void setSign_type(String sign_type) {
-        this.sign_type = sign_type;
-    }
 }
