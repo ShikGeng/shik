39b5237c59b451e475487f44f67ca00994a5b265
zkgengkun@163.com <backup>
2017-07-23 10:58:02 +0800

    规范service_id

--- shik-common/src/main/java/com/shik/constant/MapConstants.java
+++ shik-common/src/main/java/com/shik/constant/MapConstants.java
@@ -21,7 +21,8 @@
  */
 package com.shik.constant;
 
-import java.util.HashMap;
+import org.apache.commons.collections.MapUtils;
+
 import java.util.Map;
 
 /**
@@ -33,35 +34,35 @@ public class MapConstants {
     /**
      * url_pass.properties
      */
-    public static Map<String, String> URL_PASS_MAP = new HashMap<String, String>();
+    public static Map<String, String> URL_PASS_MAP = MapUtils.EMPTY_MAP;
 
     /**
      * 浏览器特殊请求
      */
-    public static Map<String, String> URL_SPECIAL_MAP = new HashMap<String, String>();
+    public static Map<String, String> URL_SPECIAL_MAP = MapUtils.EMPTY_MAP;
 
     /**
      * freemarker.properties
      */
-    public static Map<String, String> FREE_MARKER_MAP = new HashMap<String, String>();
+    public static Map<String, String> FREE_MARKER_MAP = MapUtils.EMPTY_MAP;
 
     /**
      * jsoup.properties
      */
-    public static Map<String, String> JSOUP_MAP = new HashMap<String, String>();
+    public static Map<String, String> JSOUP_MAP = MapUtils.EMPTY_MAP;
 
     /**
      * book.properties
      */
-    public static Map<String, String> BOOK_MAP = new HashMap<String, String>();
+    public static Map<String, String> BOOK_MAP = MapUtils.EMPTY_MAP;
 
     /**
      * 搜索book_url
      */
-    public static Map<String, String> SEARCH_BOOK_MAP = new HashMap<String, String>();
+    public static Map<String, String> SEARCH_BOOK_MAP = MapUtils.EMPTY_MAP;
 
     /**
      * alipay.properties
      */
-    public static Map<String, String> ALIPAY_MAP = new HashMap<String, String>();
+    public static Map<String, String> ALIPAY_MAP = MapUtils.EMPTY_MAP;
 }
