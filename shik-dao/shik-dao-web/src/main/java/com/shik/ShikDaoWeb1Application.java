0d9a88f1a4ea402be76e72df6e909ff7e43ba2b5
Shik <zkgengkun@163.com>
2017-07-28 14:46:21 +0800

    添加shik-dao模块 与数据库交互

---
+++ shik-dao/shik-dao-web/src/main/java/com/shik/ShikDaoWeb1Application.java
@@ -0,0 +1,39 @@
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+package com.shik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author gengshikun
+ * @date 2017/3/24
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ShikDaoWeb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShikDaoWeb1Application.class);
+    }
+}
