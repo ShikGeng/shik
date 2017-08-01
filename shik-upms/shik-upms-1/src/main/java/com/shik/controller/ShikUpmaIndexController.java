68f5f2749a4a0e10ef5a5982da220c49cca1e394
Shik <zkgengkun@163.com>
2017-08-01 17:44:45 +0800

    upms 应用freemarker

---
+++ shik-upms/shik-upms-1/src/main/java/com/shik/controller/ShikUpmaIndexController.java
@@ -0,0 +1,48 @@
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+package com.shik.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author gengshikun
+ * @date 2017/8/1
+ */
+@Controller
+@RequestMapping
+public class ShikUpmaIndexController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ShikUpmaIndexController.class);
+
+    /**
+     * upms-index
+     * @return
+     */
+    @RequestMapping(value = {"", "/index"}, method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
