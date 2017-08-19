3be1fc3e99b6d60cbc922647f3fae0fc94d468f9
Shik <zkgengkun@163.com>
2017-08-19 17:05:22 +0800

    ShiroConfig

---
+++ shik-upms/shik-upms-common/src/main/java/com/shik/config/ShiroConfig.java
@@ -0,0 +1,69 @@
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+package com.shik.config;
+
+import org.apache.shiro.authc.Authenticator;
+import org.apache.shiro.authc.pam.AtLeastOneSuccessfulStrategy;
+import org.apache.shiro.authc.pam.ModularRealmAuthenticator;
+import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author gengshikun
+ * @date 2017/8/19
+ */
+@Configuration
+public class ShiroConfig {
+
+
+
+    @Bean
+    public DefaultWebSecurityManager securityManager(@Qualifier("ehcacheManager") CacheManager ehcacheManager,
+                                           @Qualifier("authenticator") Authenticator authenticator) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+
+        securityManager.setCacheManager(ehcacheManager);
+        securityManager.setAuthenticator(authenticator);
+
+        return securityManager;
+    }
+
+    @Bean("ehcacheManager")
+    public CacheManager ehcacheManager() {
+        EhCacheManager ehCacheManager = new EhCacheManager();
+        ehCacheManager.setCacheManagerConfigFile("classpath:ehcache.xml");
+        return ehCacheManager;
+    }
+
+    @Bean
+    public Authenticator authenticator() {
+        ModularRealmAuthenticator modularRealmAuthenticator = new ModularRealmAuthenticator();
+        AtLeastOneSuccessfulStrategy atLeastOneSuccessfulStrategy = new AtLeastOneSuccessfulStrategy();
+        modularRealmAuthenticator.setAuthenticationStrategy(atLeastOneSuccessfulStrategy);
+        return modularRealmAuthenticator;
+    }
+
+}
