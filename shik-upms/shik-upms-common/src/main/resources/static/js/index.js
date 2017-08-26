3d18139e8e8a662d6cd9c254d597ca8acb6dcc1c
Shik <zkgengkun@163.com>
2017-08-26 16:03:02 +0800

    upms 后台首页 布局及事件

--- shik-upms/shik-upms-common/src/main/resources/static/js/index.js
+++ shik-upms/shik-upms-common/src/main/resources/static/js/index.js
@@ -3,12 +3,7 @@
  * @author gengshikun
  * @date 2016/12/12
  */
-layui.config({
-    dir: 'http://static.shik.com:1180/layui/' //layui.js 所在路径（注意，如果是script单独引入layui.js，无需设定该参数。），一般情况下可以无视
-    ,version: false //一般用于更新模块缓存，默认不开启。设为true即让浏览器不缓存。也可以设为一个固定的值，如：201610
-    ,debug: false //用于开启调试模式，默认false，如果设为true，则JS模块的节点会保留在页面
-    ,base: '' //设定扩展的Layui模块的所在目录，一般用于外部模块扩展
-}).use(['layer', 'jquery', 'element', 'laypage', 'form'], function () {
+layui.use(['layer', 'jquery', 'element', 'laypage', 'form'], function () {
     var layer = layui.layer;
         element = layui.element;
         $ = layui.jquery;
