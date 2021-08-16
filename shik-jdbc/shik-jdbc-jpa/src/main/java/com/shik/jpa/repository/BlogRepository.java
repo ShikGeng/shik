3f54976351a90cdf2bad8b9baa2daa07a291a6fe
William <87509674+WilliamSESE@users.noreply.github.com>
2021-08-16 15:56:55 +0800

    Update BlogRepository.java
    Add @Repository annotation

--- shik-jdbc/shik-jdbc-jpa/src/main/java/com/shik/jpa/repository/BlogRepository.java
+++ shik-jdbc/shik-jdbc-jpa/src/main/java/com/shik/jpa/repository/BlogRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author gengshikun
  * @date 2016/12/6
  */
+@Repository
 @CacheConfig(cacheNames = "blogs")
 public interface BlogRepository extends PagingAndSortingRepository<Blog, String>, JpaRepository<Blog, String> {
 
